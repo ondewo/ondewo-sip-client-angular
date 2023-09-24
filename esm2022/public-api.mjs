890cea11c84a8bce4cee9b72917f670b3a170e0d
Andreas Rath <andreas.rath@ondewo.com>
2023-09-24 16:39:52 +0200

    Angular 16 upgrade

---
+++ esm2022/public-api.mjs
@@ -0,0 +1,4 @@
+export * from './api/ondewo/sip/sip.pbconf';
+export * from './api/ondewo/sip/sip.pb';
+export * from './api/ondewo/sip/sip.pbsc';
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoicHVibGljLWFwaS5qcyIsInNvdXJjZVJvb3QiOiIiLCJzb3VyY2VzIjpbIi4uLy4uL3B1YmxpYy1hcGkudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IkFBQUEsY0FBYyw2QkFBNkIsQ0FBQztBQUM1QyxjQUFjLHlCQUF5QixDQUFDO0FBQ3hDLGNBQWMsMkJBQTJCLENBQUMiLCJzb3VyY2VzQ29udGVudCI6WyJleHBvcnQgKiBmcm9tICcuL2FwaS9vbmRld28vc2lwL3NpcC5wYmNvbmYnO1xuZXhwb3J0ICogZnJvbSAnLi9hcGkvb25kZXdvL3NpcC9zaXAucGInO1xuZXhwb3J0ICogZnJvbSAnLi9hcGkvb25kZXdvL3NpcC9zaXAucGJzYyc7XG4iXX0=
